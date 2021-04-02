4f6be9b91e3bc80fda55bd19f54185fddd2ef881
bhshan98 <bhushanrsonawane1998@gmail.com>
2021-04-02 23:27:17 +0530

    React Blogs App - Complete app

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,15 @@
-import { useState } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My First Website', body: 'Lorem Ipsum', author: 'mario', id: 1 },
-        { title: 'Welcome', body: 'Lorem Ipsum', author: 'luigi', id: 2 },
-        { title: 'Okay', body: 'Lorem Ipsum', author: 'mario', id: 3 }
-    ]);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
-
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading ... </div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
 }
